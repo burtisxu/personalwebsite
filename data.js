5256dc87267f35d8caa37bc68f2bec0675ccd249
burtisxu <haolun_xu@outlook.com>
2026-06-26 13:15:26 +0800

    Update data.js

--- data.js
+++ data.js
@@ -32,7 +32,9 @@
     ],
     workInProgress: [
       { id: 1, title: "Innovation Characteristics of Enterprises", description: "", status: "In Progress" },
-      { id: 2, title: "Crime", description: "", status: "In Progress" }
+      { id: 2, title: "Crime", description: "", status: "In Progress" },
+      { id: 3, title: "	
+manufacturing firm productivity", description: "", status: "In Progress" }
     ],
     conferences: [
       { id: 1, title: "Firm Productivity", conference: "The 25th China Economics Annual Conference", location: "Shanghai, China", date: "2025", type: "Presentation" },
