a41262a51b2aea20fea812597b88203f9e4aa844
burtisxu <haolun_xu@outlook.com>
2026-08-06 09:51:28 +0800

    Update data.js

--- data.js
+++ data.js
@@ -26,9 +26,8 @@
     ],
     workingPapers: [
       { id: 1, title: "Firm Productivity", authors: "H.X. & C.C.", year: "2025", abstract: "", ssrn: "", fullText: "", link: "" },
-      { id: 2, title: "Entrepreneurship", authors: "C.C. & H.X.", year: "2025", abstract: "", ssrn: "", fullText: "", link: "" },
-      { id: 3, title: "Crime", authors: "S.S. & H.X.", year: "2026", abstract: "", ssrn: "", fullText: "", link: "" },
-      { id: 4, title: "Intergenerational Educational Mobility", authors: "W.F., G.L., M.L., Y.S., Y.X., H.X., J.W.", year: "2025", abstract: "", ssrn: "", fullText: "", link: "" }
+      { id: 2, title: "Crime", authors: "S.S. & H.X.", year: "2026", abstract: "", ssrn: "", fullText: "", link: "" },
+      { id: 3, title: "Intergenerational Educational Mobility", authors: "W.F., G.L., M.L., Y.S., Y.X., H.X., J.W.", year: "2025", abstract: "", ssrn: "", fullText: "", link: "" }
     ],
     workInProgress: [
       { id: 1, title: "Innovation Characteristics of Enterprises", description: "", status: "In Progress", coauthors: "S.S." },
